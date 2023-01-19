dd24498544d963189c40da0272051c923d07c617
Ashish <shinyflame007@gmail.com>
2023-01-18 23:27:10 -0500

    Added models

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A mini quadcopter
 ## Links:
 - https://www.youtube.com/watch?v=Vep09UZsuiQ
 - https://www.instructables.com/Arduino-micro-Quadcopter/
+- https://www.youtube.com/watch?v=J0h2E532X3E
+- https://www.circuitmagic.com/arduino/run-small-brushed-motor-for-mini-quadcopter/
 ### Parts:
 - https://www.amazon.ca/Upgraded-Connector-Battery-Inductrix-Eachine/dp/B07NWD5NTN/
 - https://www.amazon.ca/DaFuRui-8520-8-5x20mm-Coreless-53000Rpm/dp/B09XBBNNVR
---
+++ TestCode/MotorTest/MotorTest.ino
@@ -0,0 +1,25 @@
+#define fadePin 3
+
+void setup() {
+  pinMode(fadePin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+//  for (int i = 0; i < 360; i++) {
+//    //convert 0-360 angle to radian (needed for sin function)
+//    float rad = DEG_TO_RAD * i;
+//
+//    //calculate sin of angle as number between 0 and 255
+//    int sinOut = constrain((sin(rad) * 128) + 128, 0, 255);
+//
+//    analogWrite(fadePin, sinOut);
+//
+//    Serial.println(sinOut);
+//    delay(15);
+//  }
+
+  digitalWrite(fadePin, HIGH);
+  //analogWrite(fadePin, 255);
+  delay(5);
+}
